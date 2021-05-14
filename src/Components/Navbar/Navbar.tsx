560ae72c8dc46627f69d70d624e541c3de5248e6
pilotwriter <yahya.emre.96@gmail.com>
2021-05-14 14:04:45 +0200

    added mainpage navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
-
-export default function Navbar() {
+import './Navbar.css'
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AddIcon from '@material-ui/icons/Add';
+const Navbar: React.FC = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
+        <div className="navbarContainer">
+            <div className="navbarLeft">
+                <button id="sidebarToggle">
+                <MenuIcon id="navbarMenuIcon"/>
+                </button>
+                <YouTubeIcon id="youtubeIcon" style={{color:"red"}}/>
+                <h1>YouTube</h1>
+            </div>
+            <div className="navbarCenter">
+                <input id="searchBar" placeholder="Search" />
+                <button id="searchButton"> <SearchIcon style={{color:"grey"}} /></button>
+            </div>
+            <div className="navbarRight">
+                <button id="addVideoButton">
+                <AddIcon id="addVideoIcon"/>
+                </button>
+                <button id="profilePic"></button>
+            </div>
         </div>
     )
 }
+
+export default Navbar;
--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
-export default function Sidebar() {
+// FC = functional Component
+const Sidebar: React.FC = () => {
     return (
         <div style={{width:250}}>
             <h1>Sidebar</h1>
         </div>
     )
 }
+
+export default Sidebar;
